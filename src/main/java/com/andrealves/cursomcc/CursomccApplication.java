acbfd12dc5e1f5ac8aa2eabd8a37917e4192c753
André Alves <andrealvesdc@gmail.com>
2019-08-04 15:53:24 -0300

    Projeto criado

---
+++ src/main/java/com/andrealves/cursomcc/CursomccApplication.java
@@ -0,0 +1,13 @@
+package com.andrealves.cursomcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrealves/cursomcc/CursomccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrealves.cursomcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
