5ed9409ec22885e689e5f9e6e9dbb46c2969a584
André Alves <andrealvesdc@gmail.com>
2019-08-04 16:42:17 -0300

    Testando aplimeira classe de dominio - categoria

--- src/main/java/com/andrealves/cursomcc/resources/CategoriaResources.java
+++ src/main/java/com/andrealves/cursomcc/resources/CategoriaResources.java
@@ -1,16 +1,29 @@
 package com.andrealves.cursomcc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.andrealves.cursomcc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
