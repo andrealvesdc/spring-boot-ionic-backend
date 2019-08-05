1b20d9376345e8304309e36899765e2881ee2942
André Alves <andrealvesdc@gmail.com>
2019-08-05 12:10:15 -0300

    Correção busca por ID

--- src/main/java/com/andrealves/cursomcc/services/CategoriaService.java
+++ src/main/java/com/andrealves/cursomcc/services/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.andrealves.cursomcc.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +15,8 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {
-		Categoria obj = repo.getOne(id);
-		return obj;
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
 	}
 
 }
