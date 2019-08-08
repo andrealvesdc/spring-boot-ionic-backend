b9dd72095a5958234d736ec0ba06c78cb641ad19
André Alves <andrealvesdc@gmail.com>
2019-08-08 15:19:04 -0300

    Criando operaco de instanciação

--- src/main/java/com/andrealves/cursomcc/CursomccApplication.java
+++ src/main/java/com/andrealves/cursomcc/CursomccApplication.java
@@ -1,13 +1,33 @@
 package com.andrealves.cursomcc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.andrealves.cursomcc.domain.Categoria;
+import com.andrealves.cursomcc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomccApplication {
+public class CursomccApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomccApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
