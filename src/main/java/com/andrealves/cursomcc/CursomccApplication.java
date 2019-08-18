827dd97e27362a09b411283042bdceb4a112981d
André Alves <andrealvesdc@gmail.com>
2019-08-17 22:47:55 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/andrealves/cursomcc/CursomccApplication.java
+++ src/main/java/com/andrealves/cursomcc/CursomccApplication.java
@@ -1,5 +1,6 @@
 package com.andrealves.cursomcc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.andrealves.cursomcc.domain.Cidade;
 import com.andrealves.cursomcc.domain.Cliente;
 import com.andrealves.cursomcc.domain.Endereco;
 import com.andrealves.cursomcc.domain.Estado;
+import com.andrealves.cursomcc.domain.Pagamento;
+import com.andrealves.cursomcc.domain.PagamentoComBoleto;
+import com.andrealves.cursomcc.domain.PagamentoComCartao;
+import com.andrealves.cursomcc.domain.Pedido;
 import com.andrealves.cursomcc.domain.Produto;
+import com.andrealves.cursomcc.domain.enums.EstadoPagamento;
 import com.andrealves.cursomcc.domain.enums.TipoCliente;
 import com.andrealves.cursomcc.repositories.CategoriaRepository;
 import com.andrealves.cursomcc.repositories.CidadeRepository;
 import com.andrealves.cursomcc.repositories.ClienteRepository;
 import com.andrealves.cursomcc.repositories.EnderecoRepository;
 import com.andrealves.cursomcc.repositories.EstadoRepository;
+import com.andrealves.cursomcc.repositories.PagamentoRepository;
+import com.andrealves.cursomcc.repositories.PedidoRepository;
 import com.andrealves.cursomcc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,7 +44,10 @@ public class CursomccApplication implements CommandLineRunner{
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomccApplication.class, args);
@@ -86,6 +97,21 @@ public class CursomccApplication implements CommandLineRunner{
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:32"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 
 }
