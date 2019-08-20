f8dd71af73b8db0eba969a9f33c7d0a0e6c244ac
André Alves <andrealvesdc@gmail.com>
2019-08-20 16:02:44 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/andrealves/cursomcc/resources/CategoriaResources.java
+++ src/main/java/com/andrealves/cursomcc/resources/CategoriaResources.java
@@ -1,6 +1,8 @@
 package com.andrealves.cursomcc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andrealves.cursomcc.domain.Categoria;
+import com.andrealves.cursomcc.dto.CategoriaDTO;
 import com.andrealves.cursomcc.services.CategoriaService;
 
 @RestController
@@ -47,5 +50,12 @@ public class CategoriaResources {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 		
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() throws Exception {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
--- src/main/java/com/andrealves/cursomcc/services/CategoriaService.java
+++ src/main/java/com/andrealves/cursomcc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.andrealves.cursomcc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +44,10 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que contem produtos");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 
 
